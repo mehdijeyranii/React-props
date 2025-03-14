164407c8be9dc907dd66da7d5eed0c466387ccf8
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-14 20:21:35 +0330

    Send a string as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+const Greeting = ({ name }: { name: string }) => {
+  return <h2>Hello, {name}</h2>;
+};
+
 const App = () => {
   return (
     <div>
       <h1>Hello, World!</h1>
+      <hr />
+      <Greeting name="Mehdi"/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
