7feea6818fbd6a6f5645169b7facd0039962e250
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-14 20:26:02 +0330

    Send a number as a prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ const Greeting = ({ name }: { name: string }) => {
   return <h2>Hello, {name}</h2>;
 };
 
+function Age({ age }: { age: number }) {
+  return <p>Your age is {age}</p>;
+}
+
 const App = () => {
   return (
     <div>
       <h1>Hello, World!</h1>
       <hr />
-      <Greeting name="Mehdi"/>
+      <Greeting name="Mehdi" />
+      <Age age={34} />
     </div>
   );
 };
