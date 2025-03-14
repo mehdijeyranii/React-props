f281be07cdb13ada2ae3d2ef0ad31ee6fa3ca4a3
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-14 20:29:12 +0330

    Send a boolean as a prop

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ function Age({ age }: { age: number }) {
   return <p>Your age is {age}</p>;
 }
 
+function Status({ isOnline }: { isOnline: boolean }) {
+  return <p>Status: {isOnline ? "Online" : "Offline"}</p>;
+}
+
 const App = () => {
   return (
     <div>
@@ -13,6 +17,8 @@ const App = () => {
       <hr />
       <Greeting name="Mehdi" />
       <Age age={34} />
+      <Status isOnline />
+      <Status isOnline={false} />
     </div>
   );
 };
