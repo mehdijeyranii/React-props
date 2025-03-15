6b417c454cb7c4a1600d45e5eb06bd4bb0bab1df
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-15 11:41:25 +0330

    Exercise 1: Display user information

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,25 @@
-import { ReactNode } from "react";
-
-const Greeting = ({ name }: { name: string }) => {
-  return <h2>Hello, {name}</h2>;
-};
-
-function Age({ age }: { age: number }) {
-  return <p>Your age is {age}</p>;
-}
-
-function Status({ isOnline }: { isOnline: boolean }) {
-  return <p>Status: {isOnline ? "Online" : "Offline"}</p>;
-}
-
-const UserInfo = ({
+const UserCard = ({
   name,
   age,
-  isOnline,
+  email,
 }: {
   name: string;
   age: number;
-  isOnline: boolean;
+  email: string;
 }) => {
   return (
     <div>
-      <h3>Name: {name}</h3>
-      <p>Age: {age}</p>
-      <p style={{ backgroundColor: isOnline ? "green" : "transparent" }}>
-        Status: {isOnline ? "Online" : "Offline"}
-      </p>
+      <h1>Name: {name}</h1>
+      <p>Your age is {age}</p>
+      <p>Your email is {email}</p>
     </div>
   );
-};
-
-const ShoppingList = ({ items }: { items: string[] }) => {
-  return (
-    <ul>
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
-  );
-};
-
-const Profile = ({ user }: { user: { name: string; age: number } }) => {
-  return (
-    <div>
-      <h3>Name: {user.name}</h3>
-      <p>Age: {user.age}</p>
-    </div>
-  );
-};
-
-const Button = ({ onClick }: { onClick: () => void }) => {
-  return <button onClick={onClick}>Click Me!</button>;
-};
-
-const Card = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
 };
 
 const App = () => {
-  const userData = { name: "Milad Khojaste", age: 35 };
-
-  const handleClick = () => {
-    alert("Button Clicked!");
-  };
-
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <hr />
-      <Greeting name="Mehdi" />
-      <Age age={34} />
-      <Status isOnline />
-      <Status isOnline={false} />
-      <UserInfo name="Ali" age={24} isOnline={true} />
-      <UserInfo name="Sara" age={24} isOnline={false} />
-      <hr />
-      <ShoppingList items={["Apple", "Banana", "Orange"]} />
-      <hr />
-      <Profile user={userData} />
-      <hr />
-      <Button onClick={handleClick} />
-      <hr />
-      <Card>
-        <h2>Title</h2>
-        <p>This is a description inside the card</p>
-      </Card>
+      <UserCard name="Ali" age={25} email="ali34@gmail.com" />
     </div>
   );
 };
