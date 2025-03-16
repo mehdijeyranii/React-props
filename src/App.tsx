b1d3d90ad9aea8ffd83e6ddda686d503fb0c02f5
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-16 12:46:15 +0330

    Implement user profile management using object state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
 import { useState } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(() => {
-    console.log("Initializing...");
-    return 10;
+interface UserProps {
+  name: string;
+  age: number;
+  city: string;
+}
+
+const UserProfile = () => {
+  const [user, setUser] = useState<UserProps>({
+    name: "Ali",
+    age: 25,
+    city: "Tehran",
   });
+
+  const changeName = () => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      name: "Mehdi",
+    }));
+  };
+
+  const increaseAge = () => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      age: prevUser.age + 1,
+    }));
+  };
+
+  const changeCity = (newCity: string) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      city: newCity,
+    }));
+  };
 
   return (
     <div>
-      <p>Current number: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <h2>User information</h2>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <p>City: {user.city}</p>
+      <hr />
+      <button onClick={changeName}>Change Name</button>
+      <button onClick={increaseAge}>Increasing Age</button>
+      <button onClick={() => changeCity("Shiraz")}>
+        Change the city of Shiraz
+      </button>
     </div>
   );
 };
@@ -17,7 +53,7 @@ const Counter = () => {
 const App = () => {
   return (
     <div>
-      <Counter />
+      <UserProfile />
     </div>
   );
 };
