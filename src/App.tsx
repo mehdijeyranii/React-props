9ee496beaf38ba6fd2cefb50938256140ff82663
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-16 13:55:02 +0330

    Add removeItem function to delete items from the list and update localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 const App = () => {
   const savedItems = localStorage.getItem("items");
-  const initialItems = savedItems ? JSON.parse(savedItems) : [];
+  const initialItems = savedItems ? JSON.parse(savedItems) : [1, 2, 3];
   const [items, setItems] = useState<number[]>(initialItems);
 
   const addItem = () => {
     const nextItem = items.length > 0 ? items[items.length - 1] + 1 : 1;
-    
+
     setItems((prevItem) => {
       const newItems = [...prevItem, nextItem];
       localStorage.setItem("items", JSON.stringify(newItems));
@@ -15,9 +15,14 @@ const App = () => {
     });
   };
 
+  const removeItem = (itemToRemove: number) => {
+    setItems((prevItems) => prevItems.filter((item) => item !== itemToRemove));
+  };
+
   return (
     <div>
       <button onClick={addItem}>Add Item</button>
+      <button onClick={() => removeItem(3)}>Remove Item 3</button>
       <ul>
         {items.map((item) => (
           <li key={item}>Item: {item}</li>
