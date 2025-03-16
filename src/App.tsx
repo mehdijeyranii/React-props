d65ee2a34e3d6eb6cf0cc94e332930938276b0d9
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-16 13:19:14 +0330

    Demonstrate usage of arrays in state with useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,20 @@
 import { useState } from "react";
 
-interface Post {
-  title: string;
-  content: string;
-  author: string;
-}
-
-const BlogPost = () => {
-  const [post, setPost] = useState<Post>({
-    title: "My first Post",
-    content: "This is a simple content",
-    author: "Ali",
-  });
-
-  const changeTitle = () => {
-    setPost((prevPost) => ({
-      ...prevPost,
-      title: "Updated Post Title",
-    }));
-  };
-
-  const changeContent = () => {
-    setPost((prevPost) => ({
-      ...prevPost,
-      content: "This is the new content for the post.",
-    }));
-  };
-
-  const changeAuthor = (newAuthor: string) => {
-    setPost((prevPost) => ({
-      ...prevPost,
-      author: newAuthor,
-    }));
-  };
-
-  return (
-    <div>
-      <h2>Blog Post</h2>
-      <p>Title: {post.title}</p>
-      <p>Content: {post.content}</p>
-      <p>Author: {post.author}</p>
-      <hr />
-      <button onClick={changeTitle}>Change Title</button>
-      <button onClick={changeContent}>Change Content</button>
-      <button onClick={() => changeAuthor("Mehdi")}>
-        Change Author to Mehdi
-      </button>
-    </div>
-  );
-};
-
 const App = () => {
+  const [items, setItems] = useState<number[]>([1, 2, 3]);
+
+  const addItem = () => {
+    setItems((prevItem) => [...prevItem, 4]);
+  };
+
   return (
     <div>
-      <BlogPost />
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>Item: {item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
